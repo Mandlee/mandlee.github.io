9587ad27c04d24ccd206265f3cc61ed72f623964
Bence Mándli <mandli.bence@gmail.com>
2018-09-04 18:54:50 +0200

    Upate content and rewrite with tailwind

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,111 +1,81 @@
 import React from 'react';
-import { FaSuitcase, FaGraduationCap } from 'react-icons/fa';
 
-const iconSettings = {
-    size: '4em',
-};
+const experiences = [
+    {
+        company: 'EPAM Systems',
+        title: 'Senior Frontend Developer',
+        date: 'Nov 2020 - Present',
+        description:
+            "I'm currently working as a UIE developer. My job consist of implementing new features and maintaining existing websites for key customer in hospitality industry.",
+    },
+    {
+        company: 'Hearsay Systems',
+        title: 'Senior Frontend Developer',
+        date: 'Apr 2019 - Nov 2020',
+        description:
+            "I've worked as a Frontend developer on Sites product. Hearsay Sites is the easiest and fastest way for creating beautiful and high converting agent websites for the financial services industry. My main responsibilities were developing next generation of Sites product, implementing new features and maintaining existing websites for key financial customers.",
+    },
+    {
+        company: 'HBO Europe',
+        title: 'Senior Frontend Developer',
+        date: 'May 2017 - Mar 2019',
+        description:
+            "I worked on HBO GO's web platform through Attrecto Next Tech Digital Solutions. HBO GO is a web streaming platform, which is available in more than 10 countries in Europe. I was part of the team, which developed the new version of HBO GO's frontend. My main responsibilities were implementing new features and coding the final look and feel of the site.",
+    },
+    {
+        company: 'Attrecto Next Tech Digital Solutions',
+        title: 'Frontend Developer',
+        date: 'Dec 2014 - Mar 2019',
+        description:
+            'Developed individual executive information systems with Javascript language, AngularJS framework and creating sitebuild for the screens with Sass, CSS. Participation in international, multi-team agile development projects, for US customer. The aim of these projects to visualize the data with charts to help customers to make a right decision.',
+    },
+    {
+        company: 'Szintézis Informatika Zrt.',
+        title: 'Android Developer Intern',
+        date: 'Apr 2014 - Sep 2014',
+        description:
+            "Developed an Android application for Széchenyi István University that presented the university's infrastructure. My responsibilities included software design, implementation, testing and writing documentation.",
+    },
+];
 
 const Experience = () => {
     return (
-        <section id="experience" className="experience card">
-            <h2>Experience & Education</h2>
-            <ul className="timeline">
-                <li>
-                    <span className="icon">
-                        <FaSuitcase {...iconSettings} />
-                    </span>
-                    <div className="animated" data-animate-type="fadeInLeft">
-                        <h3>EPAM Systems</h3>
-                        <h4 className="job-title">Senior Frontend Developer</h4>
-                        <div className="date">Nov 2020 - Present</div>
-                        <p>
-                            I'm currently working as a Frontend developer. My job consist of implementing new features
-                            and maintaining existing websites for key customer in hospitality industry.
-                        </p>
+        <>
+            <section id="experience">
+                <h2>Work Experience</h2>
+                {experiences.map(({ title, date, company, description }) => (
+                    <article className="md:grid grid-cols-3 gap-4 py-2" key={company}>
+                        <div className="">
+                            <time className="leading-8">{date}</time>
+                        </div>
+                        <div className="col-span-2">
+                            <h3 className="text-xl leading-8 font-semibold text-indigo-500">{company}</h3>
+                            <h4 className="text-l pb-1 font-medium text-slate-500">{title}</h4>
+                            <p className="tracking-wide">{description}</p>
+                        </div>
+                    </article>
+                ))}
+            </section>
+            <section id="education">
+                <h2>Education</h2>
+                <article className="md:grid grid-cols-3 gap-4 py-2">
+                    <div className="">
+                        <time className="leading-8">Sep 2011 - Jan 2016</time>
                     </div>
-                </li>
-                <li>
-                    <span className="icon">
-                        <FaSuitcase {...iconSettings} />
-                    </span>
-                    <div className="animated" data-animate-type="fadeInRight">
-                        <h3>Hearsay Systems</h3>
-                        <h4 className="job-title">Senior Frontend Developer</h4>
-                        <div className="date">Apr 2019 - Nov 2020</div>
-                        <p>
-                            I've worked as a Frontend developer on Sites product. Hearsay Sites is the easiest and
-                            fastest way for creating beautiful and high converting agent websites for the financial
-                            services industry. My main responsibilities are developing next generation of Sites product,
-                            implementing new features and maintaining existing websites for key financial customers.
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <span className="icon">
-                        <FaSuitcase {...iconSettings} />
-                    </span>
-                    <div className="animated" data-animate-type="fadeInLeft">
-                        <h3>HBO Europe</h3>
-                        <h4 className="job-title">Senior Frontend Developer</h4>
-                        <div className="date">May 2017 - Mar 2019</div>
-                        <p>
-                            I worked as a frontend developer on HBO GO's web platform through Attrecto Next Tech Digital
-                            Solutions. HBO GO is a web streaming platform, which is available in more than 10 countries
-                            in Europe. I was part of the team, which developed the new version of HBO GO's frontend. My
-                            main responsibilities were implementing new features and coding the final look and feel of
-                            the site.
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <span className="icon">
-                        <FaSuitcase {...iconSettings} />
-                    </span>
-                    <div className="animated" data-animate-type="fadeInRight">
-                        <h3>Attrecto Next Tech Digital Solutions</h3>
-                        <h4 className="job-title">Frontend Developer</h4>
-                        <div className="date">Dec 2014 - Mar 2019</div>
-                        <p>
-                            Developed individual executive information systems with Javascript language, AngularJS
-                            framework and creating sitebuild for the screens with Sass, CSS. Participation in
-                            international, multi-team agile development projects, for US customer. The aim of these
-                            projects to visualize the data with charts to help customers to make a right decision.
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <span className="icon">
-                        <FaSuitcase {...iconSettings} />
-                    </span>
-                    <div className="animated" data-animate-type="fadeInLeft">
-                        <h3>Szintézis Informatika Zrt.</h3>
-                        <h4 className="job-title">Android Developer Intern</h4>
-                        <div className="date">Apr 2014 - Sep 2014</div>
-                        <p>
-                            Developed an Android application for Széchenyi István University that presented the
-                            university's infrastructure. My responsibilities included software design, implementation,
-                            testing and writing documentation.
-                        </p>
-                    </div>
-                </li>
-
-                <li id="university">
-                    <span className="icon">
-                        <FaGraduationCap {...iconSettings} />
-                    </span>
-                    <div className="animated" data-animate-type="fadeInRight">
-                        <h3>Széchenyi István University</h3>
-                        <h4 className="job-title">Bachelor's Degree, Engineering Information Technology</h4>
-                        <div className="date">Sep 2011 - Jan 2016</div>
-                        <p>
+                    <div className="col-span-2">
+                        <h3 className="text-xl leading-8 font-semibold text-indigo-500">Széchenyi István University</h3>
+                        <h4 className="text-l pb-1 font-medium text-slate-500">
+                            Bachelor's Degree, Engineering Information Technology
+                        </h4>
+                        <p className="tracking-wide">
                             Thesis: Creation of an Android application promoting and supporting healthy nutrition and
                             lifestyle. The purpose of the project was to showcase the newly released Material Design
                             elements and the steps required to create and Android Application from scratch.
                         </p>
                     </div>
-                </li>
-            </ul>
-        </section>
+                </article>
+            </section>
+        </>
     );
 };
 export default Experience;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,46 @@
 import React from 'react';
-import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Experience from '../components/Experience';
-import Skills from '../components/skills';
-import Contact from '../components/Contact';
-import Projects from '../components/Projects';
+import Skills from '../components/Skills';
 import Meta from '../components/Meta';
-import { IconContext } from 'react-icons';
+import { VscGithubAlt } from 'react-icons/vsc';
+import { TiSocialLinkedin } from 'react-icons/ti';
 
-const iconSettings = {
-    className: 'react-icons d-print-none',
-    size: '3em',
-};
+const footerIcons = [
+    {
+        icon: <VscGithubAlt />,
+        link: 'https://github.com/Mandlee',
+    },
+    {
+        icon: <TiSocialLinkedin />,
+        link: 'https://www.linkedin.com/in/bencemandli/',
+    },
+];
 
 const HomePage = () => {
     return (
-        <IconContext.Provider value={iconSettings}>
+        <>
             <Meta />
-            <Header />
-            <div className="container text-center">
-                <Hero />
-                <Experience />
-                <Skills />
-                <Projects />
+            <div className="bg-slate-100 md:p-8 p-2 min-h-screen">
+                <div className="container mx-auto p-4 md:p-16 bg-white max-w-4xl drop-shadow print:drop-shadow-none">
+                    <Hero />
+                    <Experience />
+                    <Skills />
+                </div>
+                <footer className="print:hidden">
+                    <p className="text-center py-4 text-xs">
+                        &copy; 2022 Bence Mandli - Handcrafted with Gatsby and Tailwind
+                    </p>
+                    <div className="flex items-center justify-center pb-4 text-xl">
+                        {footerIcons.map(({ icon, link }) => (
+                            <a href={link} target="_blank" rel="noreferrer" className="px-2 hover:text-indigo-500">
+                                {icon}
+                            </a>
+                        ))}
+                    </div>
+                </footer>
             </div>
-            <Contact />
-        </IconContext.Provider>
+        </>
     );
 };
 
