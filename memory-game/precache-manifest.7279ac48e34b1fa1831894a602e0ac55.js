91ee9a2b3119cba745acf871eaa51f33d0835b54
Bence Mándli <mandli.bence@gmail.com>
2018-11-23 10:24:43 +0100

    Update site, and build memory game

---
+++ memory-game/precache-manifest.7279ac48e34b1fa1831894a602e0ac55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a42ff00dc6f5191b741c7e0e3b696f3",
+    "url": "/memory-game/static/media/supercharge-logo.0a42ff00.svg"
+  },
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "67596f0ea050cd963dad",
+    "url": "/memory-game/static/js/main.67596f0e.chunk.js"
+  },
+  {
+    "revision": "1e807f0e2adb98a58cc6",
+    "url": "/memory-game/static/js/1.1e807f0e.chunk.js"
+  },
+  {
+    "revision": "67596f0ea050cd963dad",
+    "url": "/memory-game/static/css/main.c99e6686.chunk.css"
+  },
+  {
+    "revision": "e691a6c78d264eda8b731977450fce7b",
+    "url": "/memory-game/index.html"
+  }
+];
